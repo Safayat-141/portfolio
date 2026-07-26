a9799cf508260a2bf0397855523e48c489efc64a
Md Safayatur Rahman <2024133141@student.sust.edu>
2026-07-26 14:27:52 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,33 @@
 import { Routes, Route } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx'
+import PrivateRoute from './components/PrivateRoute.jsx'
 import Home from './pages/Home.jsx'
 import About from './pages/About.jsx'
 import Work from './pages/Work.jsx'
 import WorkDetail from './pages/WorkDetail.jsx'
 import Contact from './pages/Contact.jsx'
 import Admin from './pages/Admin.jsx'
+import Login from './pages/Login.jsx'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/work" element={<Work />} />
-      <Route path="/work/:slug" element={<WorkDetail />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="/admin" element={<Admin />} />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/work/:slug" element={<WorkDetail />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/admin/login" element={<Login />} />
+        <Route
+          path="/admin"
+          element={
+            <PrivateRoute>
+              <Admin />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   )
 }
