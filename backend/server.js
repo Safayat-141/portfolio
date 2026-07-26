31b600479300dabb1e0ac50e350a4c3076306fde
Md Safayatur Rahman <2024133141@student.sust.edu>
2026-07-26 10:08:37 +0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,18 +4,16 @@ const mongoose = require('mongoose');
 const connectDB = require('./config/db');
 const cookieParser = require('cookie-parser');
 const authRoutes = require('./routes/auth');
-
+const projectRoutes = require('./routes/projects');
+const aboutRoutes = require('./routes/about');
 const app = express();
-
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true,
 }));
 app.use(express.json());
 app.use(cookieParser());
-
 connectDB();
-
 app.get('/health', async (req, res) => {
   try {
     await mongoose.connection.db.admin().ping();
@@ -24,8 +22,8 @@ app.get('/health', async (req, res) => {
     res.status(500).json({ status: 'error', db: 'disconnected', error: err.message });
   }
 });
-
 app.use('/api/auth', authRoutes);
-
+app.use('/api/projects', projectRoutes);
+app.use('/api/about', aboutRoutes);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
